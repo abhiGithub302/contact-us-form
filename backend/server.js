8e952c0345caade0d4e645fa4b003144dd9d0876
abhishek <abhi19982014@gmail.com>
2024-04-16 16:26:57 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors({
     origin: 'https://contact-us-form-frontend-pearl.vercel.app'
   }));
   
-app.post('/api/download', (req, res) => {
+app.post('/api/download/', (req, res) => {
     res.download("Setup-ConnectApp.exe");
    
 });
